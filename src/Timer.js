c96ccf5c6a0a4219c9d86d35ab8f33e29909b073
Sura Al-jilawi <suraaljailawi@gmail.com>
2020-01-20 22:31:04 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -19,7 +19,7 @@ class Timer extends Component {
 
     const { time, color, className } = this.state
     return (
-      <section className="Timer" style={{background: color}}>
+      <section  style={{background: color}}>
 
         <h1>{ time }</h1>
         <button onClick={ this.stopClock }>Stop</button>
